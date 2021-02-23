f3dcf32c1c56bb51e0ea5748f3b96f2f1b043f72
blop <morgan.grt@outlook.fr>
2021-02-23 12:29:07 +0100

    update repo

--- push_to_repo.sh
+++ push_to_repo.sh
@@ -9,7 +9,7 @@ if [[ -z "$1" ]]
 		git commit -m "update repo"
 else
 	echo $1
-	git commit -m $1
+	git commit -m "$1"
 fi
 
 git push https://9cbef11163ae1108cbc9d2a3dd11a553a5954695@github.com/morgan-grt/ansible_project.git
