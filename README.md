c3f2b8a8e6d8222e3e824e93107236a27e000f94
blop <morgan.grt@outlook.fr>
2021-03-29 14:33:39 +0200

    update repo

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# ansible_project
+GUERET Morgan  
+CARDOSO Simon  
+
+***
+
+# Projet annuel
+
+Ce projet consiste en la mise en place d'un ensemble de serveurs web exécutant *nodejs* pouvant recevoir des  
+tâches à l'aide de *Ansible*.  
+Le projet permet de présenter l'automatisation de tâches en direction d'un ensemble de machines (serveurs)  
+Les fichiers et dossiers disponibles permettent la mise en place et le déploiement de notre projet annuel.  
+
+## Organisation :
+
+Le dossier comporte:
+- *ansible/* : contient les différents fichiers de configuration pour *ansible* ainsi que les playbooks  
+- *docker/* : contient le fichier de création des *dockers*  
+- *update_dockers_ips/* : contient le code *python* ainsi que les templates pour mettre à jour les ips  
+
+- *pj_ansible.gns3project* : le projet gns3 en version portable  
+- *run.sh* : script principal, permet de lancer les playbooks mais aussi de mettre à jour les ips selon les  
+paramètres passés (*./run.sh -h* : pour afficher l'aide)
+- *util.txt* : ensemble de commandes utiles pour  le bon fonctionnement au cas où
+
+## Lancement :
+
+Pour utiliser notre projet il faut pour commencer 
+- construire le *docker* pour les serveurs :  
+``` bash
+cd docker/
+docker build -t ubuntu_nodejs .
+```  
+- ajouter à gns3 le *docker* créé dans les préférences (avec 2 adapteurs) puis importer notre projet gns3.  
+
+- mettre à jour les adresses ips en lançant *run.sh* avec le paramètre *-i*  
+``` bash
+./run.sh -i
+```  
+
+- démarrer toutes les machines dans gns3 (ensuite atteindre quelques secondes)
+
+- vous pouvez maintenant utiliser les playbooks *ansible*  
+Le plus simple est d'exécuter le fichier *run.sh* avec les paramètres souhaités (voir *./run.sh -h*)  
+
+A noter : il peut y avoir un problème avec l'exécution du script si par exemple les servers nodejs sont  
+arrêtés et que nous demandons de les arrêter à nouveau  
+Ou bien si nous demandons de supprimer les processus avant le déploiement  
+  
--- run.sh
+++ run.sh
@@ -1,59 +1,73 @@
 #!/bin/bash
 
-cd ansible/
 
-message="Select a parameter in the list below:\n";
-message+="\t-i : ips update\n";
-message+="\t/!\\ Important if there has been an update in gns3 project\n";
-message+="\t/!\\ execute before project start\n\n";
-message+="\t-r : run the nodejs servers\n";
-message+="\t-d : deploy the nodejs servers\n";
-message+="\t-s : stop the nodejs servers\n";
-message+="\t-u : update the nodejs servers\n";
-message+="\t-c : delete the nodejs servers process";
 
-if [ -z "$1" ]
-	then
-		echo "Missing parameter";
-		echo -e "${message}";
-else
+printHelp() {
+	cat << EOF
+	
+Select a parameter in the list below:
+	-i : ips update\n";
+	/!\\ Important if there has been an update in gns3 project
+	/!\\ execute before project start
 
-	if [[ "$1" == '-r' ]]
-		then
-			echo -e "Parameter: $1\nAction: nodejs servers start\n";
-			ansible-playbook run_nodejs.yml;
-			
-	elif [[ "$1" == '-s' ]]
-		then
-			echo -e "Parameter: $1\nAction: nodejs servers stop\n";
-			ansible-playbook stop_nodejs.yml;
-			
-	elif [[ "$1" == '-d' ]]
-		then
-			echo -e "Parameter: $1\nAction: nodejs servers deployment\n";
-			ansible-playbook deploy_nodejs.yml;
-			
-	elif [[ "$1" == '-u' ]]
-		then
-			echo -e "Parameter: $1\nAction: index.html file update\n";
-			ansible-playbook update_nodejs.yml;
-			
-	elif [[ "$1" == '-c' ]]
-		then
-			echo -e "Parameter: $1\nAction: nodejs process cleaning\n";
-			ansible-playbook delete_process_nodejs.yml;
-			
-	elif [[ "$1" == '-i' ]]
-		then
-			echo -e "Parameter: $1\nAction: ips update\n";
-			echo '' > ~/.ssh/known_hosts;
-			cd ../update_dockers_ips/;
-			python3 update_interfaces.py;
-			
-	else
-		echo "Bad parameter";
-		echo -e "${message}";
-	fi
+	-r : run the nodejs servers
+	-d : deploy the nodejs servers
+	-s : stop the nodejs servers
+	-u : update the nodejs servers
+	-c : delete the nodejs servers process
+	
+Can run multiple parameters like : ./run.sh -ds or -d -s
+Will deploy then stop the nodejs servers
+
+EOF
+}
+
+callPlaybook() {
+	cd ansible/
+	echo -e "Parameter: $1\nAction: $3\n";
+	ansible-playbook $2.yml;
+	cd ..;
+}
+
+
+if [[ $1 == "" ]]; then
+	echo "Missing parameter";
+	printHelp;
+	exit 0;
 fi
 
-echo "done";
+while getopts "rsducih" option
+do
+	echo "--------------";
+	case $option in
+		r)
+			callPlaybook $option "run_nodejs" "nodejs servers start";;
+				
+		s)
+			callPlaybook $option "stop_nodejs" "nodejs servers stop";;
+				
+		d)	
+			callPlaybook $option "deploy_nodejs" "nodejs servers deployment";;
+				
+		u)
+			callPlaybook $option "update_nodejs" "index.html file update";;
+				
+		c)
+			callPlaybook $option "delete_process_nodejs" "nodejs process cleaning";;
+				
+		i)
+			echo -e "Parameter: $option\nAction: ips update\n";
+			echo '' > ~/.ssh/known_hosts;
+			cd update_dockers_ips/;
+			python3 update_interfaces.py;
+			cd ..;;
+				
+		h)
+			echo -e "Parameter: $option\nAction: display help";
+			printHelp;;
+	esac
+	echo "task done"
+done
+
+echo "all done";
+exit 0;
